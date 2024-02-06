c6f6caf48af20cd40b76870f314e4917026269a0
Ashokumarpasala <adinarayanapasala9Wgmail.com>
2024-02-06 17:38:05 +0530

    change code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
+import EmailSentComponent from './components/EmailSentComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Djano......</h1>
-      </div>
+        <Router>
+          <Routes>
+            <Route exact path='/' element={ <EmailSentComponent />} />
+           </Routes>
+        </Router>
     </>
   )
 }
---
+++ src/components/EmailSentComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmailSentComponent() {
+  return (
+    <div>
+      <h1>email componetr</h1>
+    </div>
+  )
+}
+
+export default EmailSentComponent
